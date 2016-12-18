15ae58d92c79d16ebcd4afb6cb49fd87a6fcee39
lchandrakanth <lchandrakanth@systimanx.com>
2016-12-18 10:20:14 +0530

    app.js and app.css file path changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
@@ -82,6 +82,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="/js/app.js"></script>
+    <script src="{{ URL::asset('js/app.js') }}"></script>
 </body>
 </html>
